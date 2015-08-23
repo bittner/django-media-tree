4f37143f9bb8f7b3daceb5dab34db292e7df7ae3
Peter Bittner <django@bittner.it>
2015-08-24 01:23:35 +0200

    Add flake8 and PyLint configuration
    Fix flake8 complaints in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,30 @@
 # encoding=utf8
-import os
+from os.path import dirname, join
 from setuptools import setup, find_packages
 
+
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(join(dirname(__file__), fname)).read()
 
 README = read('README.rst')
 
 setup(
-    name = "django-media-tree",
-    version = "0.8.1",
-    url = 'http://github.com/samluescher/django-media-tree',
-    license = 'BSD',
-    description = "Django Media Tree is a Django app for managing your website's media files in a folder tree, and using them in your own applications.",
-    long_description = README,
+    name="django-media-tree",
+    version="0.8.1",
+    url='http://github.com/samluescher/django-media-tree',
+    license='BSD',
+    description="Django Media Tree is a Django app for managing your website's "
+                "media files in a folder tree, and using them in your own "
+                "applications.",
+    long_description=README,
 
-    author = u'Samuel Luescher',
-    author_email = 'sam at luescher dot org',
-    
-    packages = find_packages(),
+    author=u'Samuel Luescher',
+    author_email='sam at luescher dot org',
+
+    packages=find_packages(),
     include_package_data=True,
 
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
